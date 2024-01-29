3c683311fe699252c54c4c5ca43e5eaf18994570
Bukhtyar Haider <bukhtyar.haider1@gmail.com>
2024-01-30 00:44:43 +0500

    Added Chip Component

---
+++ src/content.ts
@@ -0,0 +1,22 @@
+export const nftCategories = [
+  {
+    id: 1,
+    label: "Artificial intelligent",
+  },
+  {
+    id: 2,
+    label: "Monkey",
+  },
+  {
+    id: 3,
+    label: "Illustration",
+  },
+  {
+    id: 4,
+    label: "Cars",
+  },
+  {
+    id: 5,
+    label: "Art",
+  },
+];
